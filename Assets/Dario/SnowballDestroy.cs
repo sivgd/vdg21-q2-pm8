43afc6c2fbe71f0c13a623d7d1d27e70936a56bc
weezerpinkerton <1536300@mukilteo.wednet.edu>
2020-12-09 17:21:12 -0800

    Added a scene with a player and an unfinished attack script. -Dario

---
+++ Assets/Dario/SnowballDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballDestroy : MonoBehaviour
+{
+    public bool hit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = false;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+Destroy(this.gameObject);
+
+    }
+    
+}
