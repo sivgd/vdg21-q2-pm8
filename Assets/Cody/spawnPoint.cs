441642cb06f4b52153a946d57b99d57c88679d8f
Cody-Featherstone <Cfeatherstone1002@gmail.com>
2021-01-09 12:39:24 -0800

    CF: made new map for the game and a spawnpoint for enemies

---
+++ Assets/Cody/spawnPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour
+{
+
+
+    private Vector3 playerOrigin;
+    public GameObject mainPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerOrigin = mainPlayer.transform.position;
+        transform.localPosition = playerOrigin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
