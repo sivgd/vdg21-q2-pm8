b567ca235b586ff4c9e9c52a596dc6499e92f889
Konner Lewis <1615931@mukilteo.wednet.edu>
2021-01-13 13:16:01 -0800

    Update Enemy AI.unity, Enemy AI.unity.meta, and 19 more files...

---
+++ Assets/Konner/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator myAnim;
+    private Transform target;
+    public Transform homePos;
+    [SerializeField]
+    private float speed = 0f;
+    [SerializeField]
+    private float maxRange = 0f;
+    [SerializeField]
+    private float minRange = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position)>= minRange)
+        {
+            FollowPlayer();
+        }
+        else if(Vector3.Distance(target.position, transform.position) >= maxRange)
+        {
+            GoHome();
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        myAnim.SetBool("isMoving", true);
+        myAnim.SetFloat("moveX", (target.position.x - transform.position.x));
+        myAnim.SetFloat("moveY", (target.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    public void GoHome()
+    {
+        myAnim.SetFloat("moveX", (homePos.position.x - transform.position.x));
+        myAnim.SetFloat("moveY", (homePos.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, homePos.position)==0)
+        {
+            myAnim.SetBool("isMoving", false);
+        }
+    }
+}
---
+++ Assets/Konner/HurtPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HurtPlayer : MonoBehaviour
+{
+    private float waittoLoad = 2f;
+    private bool reloading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reloading)
+        {
+            waittoLoad -= Time.deltaTime;
+            if (waittoLoad <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+    }
+}
